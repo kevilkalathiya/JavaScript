c3f439d5e31d61fceeab9c3aa9ac00e0248fb916
kevil <kevilkalathiya4266@gmail.com>
2023-07-17 18:50:09 +0530

    JavaScript

---
+++ Node/78 Modules.js
@@ -0,0 +1,7 @@
+// const { hello, ahello } = require("./Modules1"); // commonjs
+import harry, { hello, ahello } from "./Modules2.js";
+hello();
+ahello("kevil");
+ahello("jenish");
+ahello("kevil kalathiya");
+harry();
---
+++ Node/Modules2.js
@@ -0,0 +1,14 @@
+// ES6 Modules
+export const hello = () => {
+  console.log("Hello harry");
+};
+
+export const ahello = (name) => {
+  console.log("Hello " + name);
+};
+
+const harry = () => {
+  console.log("Hello harry2");
+};
+
+export default harry;
